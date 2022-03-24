3dad4d13f006115c29d0d4db900b5c16113709d9
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-03-24 15:03:02 +0000

    act

--- src/main/java/uma/requalificar/livrariarequalificar/controller/AutorController.java
+++ src/main/java/uma/requalificar/livrariarequalificar/controller/AutorController.java
@@ -1,5 +1,31 @@
 package uma.requalificar.livrariarequalificar.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import uma.requalificar.livrariarequalificar.model.Autor;
+import uma.requalificar.livrariarequalificar.service.AutorService;
+
+@RestController
+@CrossOrigin
 public class AutorController {
 
+	private final AutorService autorService;
+	
+	@Autowired
+	public AutorController (AutorService autorService)
+	{
+		this.autorService = autorService;
+	}
+	
+    @GetMapping("/getAutores")
+	@CrossOrigin
+    public List<Autor> getAutores(){
+		return autorService.getAutores();
+    }
+    
 }
--- src/main/java/uma/requalificar/livrariarequalificar/service/AutorService.java
+++ src/main/java/uma/requalificar/livrariarequalificar/service/AutorService.java
@@ -1,18 +1,31 @@
 package uma.requalificar.livrariarequalificar.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import uma.requalificar.livrariarequalificar.model.Autor;
 import uma.requalificar.livrariarequalificar.repository.AutorRepository;
 
+@Service
 public class AutorService {
 	
-	private final AutorRepository aRepository;
+	private final AutorRepository autorRepository;
 
 	@Autowired
-	public AutorService (AutorRepository aRepository)
+	public AutorService (AutorRepository autorRepository)
 	{
-		this.aRepository = aRepository;
+		this.autorRepository = autorRepository;
 	}
 	
+	public List<Autor> getAutores()
+	{
+		List<Autor> autores = new ArrayList<>();
+		autorRepository.findAll().forEach(autores::add);
 
+		return autores;
+	}
+	
 }
