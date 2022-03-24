af88b12430f486921f8a085b2c3ab8fb73cbd7e6
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-03-24 14:50:16 +0000

    act service

--- src/main/java/uma/requalificar/livrariarequalificar/service/EditoraService.java
+++ src/main/java/uma/requalificar/livrariarequalificar/service/EditoraService.java
@@ -1,5 +1,17 @@
 package uma.requalificar.livrariarequalificar.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import uma.requalificar.livrariarequalificar.repository.EditoraRepository;
+
 public class EditoraService {
 
+	private final EditoraRepository eRepository;
+
+	@Autowired
+	public EditoraService(EditoraRepository eRepository)
+	{
+		this.eRepository = eRepository;
+	}
+	
 }
--- src/main/java/uma/requalificar/livrariarequalificar/service/FuncionarioService.java
+++ src/main/java/uma/requalificar/livrariarequalificar/service/FuncionarioService.java
@@ -1,5 +1,17 @@
 package uma.requalificar.livrariarequalificar.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import uma.requalificar.livrariarequalificar.repository.FuncionarioRepository;
+
 public class FuncionarioService {
 
+	private final FuncionarioRepository fRepository;
+
+	@Autowired
+	public FuncionarioService(FuncionarioRepository fRepository)
+	{
+		this.fRepository = fRepository;
+	}
+	
 }
