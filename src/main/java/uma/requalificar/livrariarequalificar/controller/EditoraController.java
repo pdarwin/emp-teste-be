e06f3e7d103f9a450f706f0139d6dbe4985d5849
JATR22 <tr.joaoandre@gmail.com>
2022-03-25 00:51:08 +0000

    Tentativa 'Put' Editora

--- src/main/java/uma/requalificar/livrariarequalificar/controller/EditoraController.java
+++ src/main/java/uma/requalificar/livrariarequalificar/controller/EditoraController.java
@@ -8,10 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import uma.requalificar.livrariarequalificar.dto.ListaResposta;
+import uma.requalificar.livrariarequalificar.dto.SimpleResponse;
 import uma.requalificar.livrariarequalificar.model.Editora;
 import uma.requalificar.livrariarequalificar.service.EditoraService;
 
@@ -79,6 +81,21 @@ public class EditoraController
 		}
 
 	}
-  
+    
+    
+/*    @PutMapping("/updateEditora")
+    public ResponseEntity<SimpleResponse> updateEmpresa (@RequestBody Editora editora){
+        SimpleResponse sr = new SimpleResponse();
+
+        if (autorEditoraService.updateEditora(editora)){
+            sr.setSucess("Sucesso ao atualizar a empresa");
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(sr);
+        }
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sr);
+    }
+  */
     
 }
--- src/main/java/uma/requalificar/livrariarequalificar/service/EditoraService.java
+++ src/main/java/uma/requalificar/livrariarequalificar/service/EditoraService.java
@@ -2,6 +2,7 @@ package uma.requalificar.livrariarequalificar.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -42,6 +43,49 @@ public class EditoraService
 		editoraRepository.save (editora);
 		return "";
 	}
+	
+	
+/*	public boolean updateEditora(Editora editora) {
+        Optional<Editora> editoraOptional = getEditora(editora);
+        if (editoraOptional.isEmpty()){
+            return false;
+        }
+
+        Editora editoraToUpdate = editoraOptional.get();
+
+        if (editora.getNome()!= null && !editora.getNome().isBlank()){
+            editoraToUpdate.setNome(editora.getNome());
+        }
+
+        if (editora.getMorada()!= null && !editora.getMorada().isBlank()){
+            editoraToUpdate.setMorada(editora.getMorada());
+        }
+
+        editoraRepository.save(editora);
+
+        return true;
+    }
+
+	private Optional<Editora> getEditora(Editora editora) {
+		return editoraRepository.findById(editora.getId());
+		
+		if (editora.getId()!= null) {
+
+
+        ArrayList<Editora> editorasAux = new ArrayList<>();
+        editoraRepository.findAll().forEach(editorasAux::add);
+
+        for (Editora aux : editorasAux) {
+            if (aux.getId().equals(editora.getId())) {
+                return Optional.of(aux);
+            }
+
+    }
+
+    return Optional.empty();} 
+} 
+*/
+	}
 
 	
-}
+
