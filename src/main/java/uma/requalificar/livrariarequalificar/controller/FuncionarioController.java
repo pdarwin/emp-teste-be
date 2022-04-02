a48617ffc6ec14ad41fd01731cf4f766bf383261
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-04-03 00:21:59 +0100

    act

--- src/main/java/uma/requalificar/livrariarequalificar/controller/FuncionarioController.java
+++ src/main/java/uma/requalificar/livrariarequalificar/controller/FuncionarioController.java
@@ -3,10 +3,16 @@ package uma.requalificar.livrariarequalificar.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import uma.requalificar.livrariarequalificar.dto.ListaResposta;
+import uma.requalificar.livrariarequalificar.model.Funcionario;
 import uma.requalificar.livrariarequalificar.model.Funcionario;
 import uma.requalificar.livrariarequalificar.service.FuncionarioService;
 
@@ -32,5 +38,80 @@ public class FuncionarioController
 		return funcionarioService.getFuncionarios ();
     }
  
+    @PostMapping ("/loginFuncionario")
+	@CrossOrigin
+    public ResponseEntity<ListaResposta> loginFuncionario (@RequestBody Funcionario funcionario)
+    {
+    	ListaResposta listaResposta = new ListaResposta ();
+    	
+		if ( (funcionario.getNome () == null) )
+		{
+			listaResposta.addMsg ("Nome nulo.");
+			return ResponseEntity.status (HttpStatus.BAD_REQUEST).body (listaResposta);
+		}
+		
+		if ( (funcionario.getPassword () == null) )
+		{
+			listaResposta.addMsg ("Password nula.");
+			return ResponseEntity.status (HttpStatus.BAD_REQUEST).body (listaResposta);
+		}
+    	
+		listaResposta = funcionarioService.loginFuncionario (funcionario);
+		
+		if (!listaResposta.isStatusOk() )
+		{
+			return ResponseEntity.status (HttpStatus.BAD_REQUEST).body (listaResposta);
+		} 
+		else
+		{
+			return ResponseEntity.status (HttpStatus.OK).body (listaResposta);
+		}
+    }
+    
+    
+    @PostMapping ("/addFuncionario")
+	@CrossOrigin
+	public ResponseEntity<ListaResposta> addFuncionario (@RequestBody Funcionario funcionario)
+	{
+    	ListaResposta simpleResponse = new ListaResposta ();
+
+		if (funcionario.getId() != null)
+		{
+			simpleResponse.addMsg ("Ao adicionar um funcionario, o ID tem de ser nulo.");
+			return ResponseEntity.status (HttpStatus.BAD_REQUEST).body (simpleResponse);
+		}
+		
+		if ( (funcionario.getNome () == null) )
+		{
+			simpleResponse.addMsg ("Nome nulo.");
+			return ResponseEntity.status (HttpStatus.BAD_REQUEST).body (simpleResponse);
+		}
+			
+		if ( (funcionario.getPassword () == null) )
+		{
+			simpleResponse.addMsg ("Password nula.");
+			return ResponseEntity.status (HttpStatus.BAD_REQUEST).body (simpleResponse);
+		}
+		
+		if ( (funcionario.getData_nascimento () == null) )
+		{
+			simpleResponse.addMsg ("Data de nascimento nula.");
+			return ResponseEntity.status (HttpStatus.BAD_REQUEST).body (simpleResponse);
+		}
+		
+		String msg = funcionarioService.addFuncionario (funcionario);
+
+		if (!msg.isBlank () )
+		{
+			simpleResponse.addMsg (msg);
+			return ResponseEntity.status (HttpStatus.BAD_REQUEST).body (simpleResponse);
+		} 
+		else
+		{
+			simpleResponse.setStatusOk (true);
+			return ResponseEntity.status (HttpStatus.OK).body (simpleResponse);
+		}
+
+	}
     
 }
--- src/main/java/uma/requalificar/livrariarequalificar/service/FuncionarioService.java
+++ src/main/java/uma/requalificar/livrariarequalificar/service/FuncionarioService.java
@@ -1,13 +1,18 @@
 package uma.requalificar.livrariarequalificar.service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import uma.requalificar.livrariarequalificar.dto.ListaResposta;
+import uma.requalificar.livrariarequalificar.model.Funcionario;
 import uma.requalificar.livrariarequalificar.model.Funcionario;
 import uma.requalificar.livrariarequalificar.repository.FuncionarioRepository;
+import uma.requalificar.livrariarequalificar.utils.Utils;
 
 @Service
 public class FuncionarioService 
@@ -27,6 +32,83 @@ public class FuncionarioService
 		funcionarioRepository.findAll ().forEach (funcionarios::add);
 
 		return funcionarios;
+	}
+	
+	public String addFuncionario (Funcionario funcionario)
+	{
+		if (funcionario.getNome ().isBlank () )
+			return "Nome não preenchido.";
+		
+		if (funcionario.getPassword ().isBlank () )
+			return "Palavra-passe não preenchida.";
+		
+		if (!Utils.validatePassword (funcionario.getPassword () ) )
+			return "Palavra-passe inválida. A palavra-passe deve ter entre 8 e 20 caracteres, e conter pelo menos uma letra minúscula, uma letra maiúscula e um dígito";
+	
+		
+		LocalDate dataMaxima = LocalDate.now ();
+		
+		LocalDate dataMinima = dataMaxima.minusYears (120);
+		
+		if (dataMinima.compareTo (funcionario.getData_nascimento ().toLocalDate () ) > 0 || dataMaxima.compareTo (funcionario.getData_nascimento ().toLocalDate () ) <= 0 )
+			return "Data de nascimento inválida.";
+		
+		//Encriptar password
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		funcionario.setPassword (passwordEncoder.encode(funcionario.getPassword () ) );
+		
+		
+		funcionarioRepository.save (funcionario);
+		return "";
+	}
+	
+	public ListaResposta loginFuncionario (Funcionario funcionario)
+	{
+		
+		ListaResposta listaResposta = new ListaResposta ();
+		
+		if (funcionario.getNome ().isBlank () )
+		{
+			listaResposta.addMsg("Nome não preenchido.");
+			return listaResposta;
+		}
+			
+		
+		if (funcionario.getPassword ().isBlank () )
+		{
+			listaResposta.addMsg("Palavra-passe não preenchida.");
+			return listaResposta;
+		}
+		
+		for (Funcionario funcionarioAux : getFuncionarios())
+		{
+			if (funcionarioAux.getNome().equals(funcionario.getNome()))
+			{
+				//Testar password
+				BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+				if (!passwordEncoder.matches(funcionario.getPassword(), funcionarioAux.getPassword()))
+				{
+					listaResposta.addMsg("Palavra-passe errada.");
+					return listaResposta;
+				}
+				else if (!funcionarioAux.isAtivo())
+				{
+					listaResposta.addMsg("Funcionario inativo.");
+					return listaResposta;
+				}
+				else
+				{
+					listaResposta.setNewID(funcionarioAux.getId());
+					listaResposta.setStatusOk(true);
+					return listaResposta;
+				}
+
+			}
+			
+		}
+	
+		listaResposta.addMsg("Email incorreto ou inexistente.");
+		return listaResposta;
 	}
 	
 	
