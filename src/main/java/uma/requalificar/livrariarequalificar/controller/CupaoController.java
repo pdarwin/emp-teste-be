09249314a701e059f45bf032b91efd18ab3e7bf4
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-04-04 03:23:42 +0100

    + lista de cupões

--- src/main/java/uma/requalificar/livrariarequalificar/controller/CupaoController.java
+++ src/main/java/uma/requalificar/livrariarequalificar/controller/CupaoController.java
@@ -3,10 +3,14 @@ package uma.requalificar.livrariarequalificar.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import uma.requalificar.livrariarequalificar.dto.ListaResposta;
 import uma.requalificar.livrariarequalificar.model.Cupao;
 import uma.requalificar.livrariarequalificar.service.CupaoService;
 
@@ -32,5 +36,21 @@ public class CupaoController
 		return cupaoService.getCupoes ();
     }
  
-    
+    @GetMapping("/getCupoesByClienteId/{cliente_id}")
+	@CrossOrigin
+	public ResponseEntity<ListaResposta> getCupoesByClienteId(@PathVariable String cliente_id) {
+		ListaResposta listaResposta = new ListaResposta();
+
+		if (cliente_id.isBlank()) {
+			listaResposta.addMsg("O ID do cliente não pode ser nulo.");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(listaResposta);
+		}
+
+		listaResposta = cupaoService.getCupoesByClienteId(cliente_id);
+
+		listaResposta.setStatusOk(true);
+		return ResponseEntity.status(HttpStatus.OK).body(listaResposta);
+
+	}
+
 }
--- src/main/java/uma/requalificar/livrariarequalificar/service/CupaoService.java
+++ src/main/java/uma/requalificar/livrariarequalificar/service/CupaoService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import uma.requalificar.livrariarequalificar.dto.ListaResposta;
+import uma.requalificar.livrariarequalificar.model.Cupao;
 import uma.requalificar.livrariarequalificar.model.Cupao;
 import uma.requalificar.livrariarequalificar.repository.CupaoRepository;
 
@@ -30,7 +32,27 @@ public class CupaoService
 		return cupoes;
 	}
 	
-	// Regras de Negócio
+	public ListaResposta getCupoesByClienteId(String cliente_id) {
+		ListaResposta listaResposta = new ListaResposta();
+
+		try {
+			List<Cupao> cupoes = new ArrayList<>();
+			cupaoRepository.findAll().forEach(cupoes::add);
+
+			List<Cupao> cupoesByCliente = new ArrayList<>();
+			for (Cupao cupao : cupoes) {
+				if (cupao.getCliente().getId() == Long.parseLong(cliente_id)) {
+					cupoesByCliente.add(cupao);
+				}
+			}
+			listaResposta.setLista(cupoesByCliente);
+			return listaResposta;
+
+		} catch (NumberFormatException e) {
+			listaResposta.addMsg("Erro de formato no ID do cupao");
+			return listaResposta;
+		}
+	}
 
 	
 }
